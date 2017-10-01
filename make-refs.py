ec0c9460f62d6c9f61497199e9d82a739c5aa0ec
David Moncada <david_moncada@icloud.com>
2017-09-30 20:45:13 -0500

    make-refs.py: Port to Python 3, refactor it a bit.

--- make-refs.py
+++ make-refs.py
@@ -1,13 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-"""Takes as input a .csv file where refs. are stored, and builds from it a
-biblatex-compatible file which can then be used in a LaTeX doc."""
+"""
+Takes as input a .csv file where refs. are stored, and builds from it a
+biblatex-compatible file which can then be used in a LaTeX doc.
+"""
 
-import sys                  # For handling arguments from the CLI.
-import csv                  # For handling csv files.
-import re                   # For using regex.
-from os.path import exists  # For checking if files exist.
-from os import remove as rm # For removing files if an error occurred.
+import sys                    # For grabbing arguments from the stdin.
+import csv                    # For handling csv files.
+import re                     # For using regex.
+from os import remove as rm   # For removing files if an error occurs.
+from os.path import basename  # For stripping the stem from a file.
 
 entry_types = {
     'article'      : ['author', 'title', 'journaltitle', 'year', 'volume', 'number', 'pages', 'doi'],
@@ -20,21 +22,19 @@ entry_types = {
 
 longest = 'journaltitle'
 
-def make_templs(types): # types is a dict. of lists.
-    templs = {}
 
-    for _type, fields in types.iteritems():
+def make_templates(entry_types):  # entry_types is a dict. of lists.
+    templates = {}
+
+    for entry_type, fields in entry_types.items():
         indented = [field.ljust(len(longest)) for field in fields]
 
-        templs[_type] = '@{}{{{},\n  ' + \
-                        ' = {{{}}},\n  '.join(indented) + \
-                        ' = {{{}}}\n}}\n'
+        templates[entry_type] = '@{}{{{},\n  ' + \
+            ' = {{{}}},\n  '.join(indented) + \
+            ' = {{{}}}\n}}\n'
 
-    return templs
+    return templates
 
-# Removes chars. that might bother biber.
-def clean(fields):
-    return [field.replace('"', '').replace('&', '\&') for field in fields]
 
 def main(fname):
     try:
@@ -42,35 +42,38 @@ def main(fname):
              open(fname.replace('.csv', '.bib'), 'w') as fout:
 
             reader = csv.reader(fin, delimiter=',', quotechar='"')
-            templs = make_templs(entry_types)
+            templates = make_templates(entry_types)
+            regex = ',\n  .{{,{}}} = {{}}'.format(len(longest))
 
             for row in reader:
                 if row[0] in entry_types.keys():
-                    templ = templs[row[0]]
-                    fmttd = templ.format(*clean(row))
-                    fmttd = re.sub(',\n  .{,%i} = {}' % len(longest), '', fmttd)
-                    fout.write(fmttd)
+                    template = templates[row[0]]
+                    clean_row = list(map(
+                        lambda x: x.replace('"', '').replace('&', '\&'), row))
+                    formatted = template.format(*clean_row)
+                    formatted = re.sub(regex, '', formatted)
+                    fout.write(formatted)
                 else:
-                    raise ValueError('unknown entry type \'%s\'' % row[0])
+                    raise ValueError('unknown entry type <{}>'.format(row[0]))
 
     # IndexError is thrown when there are more fields than expected.
+    # ValueError is thrown when an unknown reference type is read.
     except (IndexError, ValueError) as err:
-        sys.stderr.write('error: ' + err.message + '\n')
+        sys.stderr.write('error: {}\n'.format(err.message))
         rm(fout.name)
+        return 1
+
+    except FileNotFoundError:
+        sys.stderr.write('error: no raw refs file\n')
         return 1
 
     return 0
 
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        sys.stderr.write('usage: $ python %s <fname>\n' % sys.argv[0])
+        sys.stderr.write('usage: $ python3 {} <fname>\n'.format(
+            basename(sys.argv[0])))
         raise SystemExit(1)
 
-    fname = sys.argv[1]
-
-    if not exists(fname):
-        sys.stderr.write('error: no raw refs file\n')
-        raise SystemExit(1)
-
-    sys.exit(main(fname))
-
+    sys.exit(main(sys.argv[1]))
